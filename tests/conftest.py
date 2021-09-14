8c6675fc691d80f942b63ae21f510bb9fdbd66b8
Anne Lyttle <alyttle@berkeley.edu>
2021-09-14 13:48:40 -0700

    DAMIEN-4: Adds configs, routes, tests, travis

---
+++ tests/conftest.py
@@ -0,0 +1,52 @@
+"""
+Copyright ©2021. The Regents of the University of California (Regents). All Rights Reserved.
+
+Permission to use, copy, modify, and distribute this software and its documentation
+for educational, research, and not-for-profit purposes, without fee and without a
+signed licensing agreement, is hereby granted, provided that the above copyright
+notice, this paragraph and the following two paragraphs appear in all copies,
+modifications, and distributions.
+
+Contact The Office of Technology Licensing, UC Berkeley, 2150 Shattuck Avenue,
+Suite 510, Berkeley, CA 94720-1620, (510) 643-7201, otl@berkeley.edu,
+http://ipira.berkeley.edu/industry-info for commercial licensing opportunities.
+
+IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
+THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS BEEN ADVISED
+OF THE POSSIBILITY OF SUCH DAMAGE.
+
+REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED
+"AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+"""
+
+import os
+os.environ['DAMIEN_ENV'] = 'test'  # noqa
+
+import pytest  # noqa
+import damien.factory  # noqa
+
+
+# Because app and db fixtures are only created once per pytest run, individual tests
+# are not able to modify application configuration values before the app is created.
+# Per-test customizations could be supported via a fixture scope of 'function' and
+# the @pytest.mark.parametrize annotation.
+
+@pytest.fixture(scope='session')
+def app(request):
+    """Fixture application object, shared by all tests."""
+    _app = damien.factory.create_app()
+
+    # Create app context before running tests.
+    ctx = _app.app_context()
+    ctx.push()
+
+    # Pop the context after running tests.
+    def teardown():
+        ctx.pop()
+    request.addfinalizer(teardown)
+
+    return _app
