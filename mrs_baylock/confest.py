3d5b7a138e1eddb638bda6d22891b3748ce7ff20
Paul Farestveit <pfarestveit@berkeley.edu>
2022-01-07 09:23:28 -0800

    Basic config and webdriver setup for Mrs Baylock

---
+++ mrs_baylock/confest.py
@@ -0,0 +1,54 @@
+"""
+Copyright ©2022. The Regents of the University of California (Regents). All Rights Reserved.
+Permission to use, copy, modify, and distribute this software and its documentation
+for educational, research, and not-for-profit purposes, without fee and without a
+signed licensing agreement, is hereby granted, provided that the above copyright
+notice, this paragraph and the following two paragraphs appear in all copies,
+modifications, and distributions.
+Contact The Office of Technology Licensing, UC Berkeley, 2150 Shattuck Avenue,
+Suite 510, Berkeley, CA 94720-1620, (510) 643-7201, otl@berkeley.edu,
+http://ipira.berkeley.edu/industry-info for commercial licensing opportunities.
+IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
+THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS BEEN ADVISED
+OF THE POSSIBILITY OF SUCH DAMAGE.
+REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED
+"AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+"""
+
+import os
+
+from damien.factory import create_app
+from mrs_baylock.pages.login_page import LoginPage
+from mrs_baylock.test_utils.webdriver_utils import WebDriverManager
+import pytest
+
+
+os.environ['DAMIEN_ENV'] = 'mrs_baylock'
+
+_app = create_app()
+
+ctx = _app.app_context()
+ctx.push()
+
+
+@pytest.fixture(scope='session')
+def page_objects(request):
+    driver = WebDriverManager.launch_browser()
+
+    # Define page objects
+    login_page = LoginPage(driver)
+
+    session = request.node
+    try:
+        for item in session.items:
+            cls = item.getparent(pytest.Class)
+            setattr(cls.obj, 'driver', driver)
+            setattr(cls.obj, 'login_page', login_page)
+        yield
+    finally:
+        WebDriverManager.quit_browser(driver)
+
