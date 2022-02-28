34d7987fed6468b6d15333cdba53194309853cf2
Anne Lyttle <alyttle@berkeley.edu>
2022-02-28 15:29:48 -0800

    DAMIEN-85: the nanny's room

--- src/api/departmentForms.ts
+++ src/api/departmentForms.ts
@@ -1,6 +1,10 @@
 import axios from 'axios'
 import Vue from 'vue'
 
+export function addDepartmentForm(name: string) {
+  return axios.post(`${Vue.prototype.$config.apiBaseUrl}/api/department_form/${name}`)
+}
+
 export function deleteDepartmentForm(name: string) {
   return axios.delete(`${Vue.prototype.$config.apiBaseUrl}/api/department_form/${name}`)
 }
--- src/api/evaluationTypes.ts
+++ src/api/evaluationTypes.ts
@@ -1,6 +1,10 @@
 import axios from 'axios'
 import Vue from 'vue'
 
+export function addEvaluationType(name) {
+  return axios.post(`${Vue.prototype.$config.apiBaseUrl}/api/evaluation_type/${name}`)
+}
+
 export function deleteEvaluationType(name) {
   return axios.delete(`${Vue.prototype.$config.apiBaseUrl}/api/evaluation_type/${name}`)
 }
