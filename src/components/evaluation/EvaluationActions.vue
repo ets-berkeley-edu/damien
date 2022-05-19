8ea83e5cea56a7201b04a3bf218138cfbbdce5cc
Anne Lyttle <alyttle@berkeley.edu>
2022-05-19 14:39:10 -0700

    DAMIEN-290: condenses eval table header, replaces buttons w chips

--- src/components/evaluation/EvaluationActions.vue
+++ src/components/evaluation/EvaluationActions.vue
@@ -1,147 +1,120 @@
 <template>
-  <v-row class="px-3 pt-4">
-    <v-col class="pt-2 pb-0" cols="12" sm="6">
-      <div class="d-flex flex-row flex-grow-1 align-baseline mb-4">
-        <select
-          id="select-course-actions"
-          v-model="selectedCourseAction"
-          aria-labelledby="action-option-label"
-          class="native-select-override"
-          :class="$vuetify.theme.dark ? 'dark' : 'light'"
-          :disabled="disableControls || !allowEdits"
-        >
-          <option
-            id="action-option-label"
-            :key="0"
-            disabled
-            :value="undefined"
-          >
-            Course Actions
-          </option>
-          <option
-            v-for="(action, index) in courseActions"
-            :id="`action-option-${action.value}`"
-            :key="index + 1"
-            :value="action.value"
-          >
-            {{ action.text }}
-          </option>
-        </select>
-        <v-btn
-          id="apply-course-action-btn"
-          class="mx-2"
-          color="secondary"
-          :disabled="disableControls || !allowEdits || !selectedCourseAction || !selectedEvaluationIds.length"
-          @click="applyCourseAction"
-          @keypress.enter.prevent="applyCourseAction"
-        >
-          Apply
-        </v-btn>
-      </div>
-      <div v-if="selectedCourseAction === 'duplicate'" class="mb-4">
-        <div class="d-flex align-center mt-2">
-          <PersonLookup
-            id="bulk-duplicate-instructor-lookup-autocomplete"
-            :instructor-lookup="true"
-            placeholder="Instructor name or UID"
-            :on-select-result="selectInstructor"
-            solo
-          />
-        </div>
-        <v-checkbox
-          v-model="bulkUpdateOptions.midtermFormEnabled"
-          class="text-nowrap"
-          color="tertiary"
-          :disabled="disableControls"
-          hide-details="auto"
-          label="Use midterm department forms"
-        />
-        <div class="d-flex align-center mt-2">
-          <label
-            for="bulk-duplicate-start-date"
-            class="v-label"
-            :class="$vuetify.theme.dark ? 'theme--dark' : 'theme--light'"
-          >
-            Evaluation start date:
-          </label>
-          <c-date-picker
-            v-model="bulkUpdateOptions.startDate"
-            class="mx-3"
-            :min-date="$moment($config.currentTermDates.begin).toDate()"
-            :max-date="$moment($config.currentTermDates.end).subtract(20, 'days').toDate()"
-            title-position="left"
-          >
-            <template v-slot="{ inputValue, inputEvents }">
-              <input
-                id="bulk-duplicate-start-date"
-                class="datepicker-input input-override my-0"
-                :class="$vuetify.theme.dark ? 'dark' : 'light'"
-                :disabled="disableControls"
-                maxlength="10"
-                minlength="10"
-                :value="inputValue"
-                v-on="inputEvents"
-              />
-            </template>
-          </c-date-picker>
-        </div>
-      </div>
-      <v-dialog
-        id="error-dialog"
-        v-model="errorDialog"
-        width="400"
-        role="alertdialog"
-        aria-labelledby="error-dialog-title"
-        aria-describedby="error-dialog-text"
+  <div>
+    <div class="d-flex flex-row flex-grow-1 align-baseline">
+      <select
+        id="select-course-actions"
+        v-model="selectedCourseAction"
+        aria-labelledby="action-option-label"
+        class="native-select-override"
+        :class="$vuetify.theme.dark ? 'dark' : 'light'"
+        :disabled="disableControls || !allowEdits"
       >
-        <v-card>
-          <v-card-title id="error-dialog-title" tabindex="-1">Error</v-card-title>
-          <v-card-text id="error-dialog-text" class="pt-3">{{ errorDialogText }}</v-card-text>
-          <div class="d-flex pa-2">
-            <div class="mr-2">
-              <v-btn
-                id="error-dialog-ok-btn"
-                color="primary"
-                @click="dismissErrorDialog"
-                @keypress.enter.prevent="dismissErrorDialog"
-              >
-                OK
-              </v-btn>
-            </div>
-          </div>
-        </v-card>
-      </v-dialog>
-    </v-col>
-    <v-col cols="12" sm="6">
-      <div class="d-flex flex-grow-1 align-baseline justify-end">
-        <v-btn
-          v-if="!isAddingSection"
-          id="add-course-section-btn"
-          class="text-capitalize pl-2 mt-1"
-          color="tertiary"
-          :disabled="!allowEdits"
-          text
-          @click="() => isAddingSection = true"
-          @keypress.enter.prevent="() => isAddingSection = true"
+        <option
+          id="action-option-label"
+          :key="0"
+          disabled
+          :value="undefined"
         >
-          <v-icon>mdi-plus-thick</v-icon>
-          Add Course Section
-        </v-btn>
-        <AddCourseSection
-          v-if="isAddingSection"
-          id="add-course-section"
-          :evaluations="evaluations"
-          :on-submit="addCourseSection"
-          :on-cancel="cancelAddSection"
+          Course Actions
+        </option>
+        <option
+          v-for="(action, index) in courseActions"
+          :id="`action-option-${action.value}`"
+          :key="index + 1"
+          :value="action.value"
+        >
+          {{ action.text }}
+        </option>
+      </select>
+      <v-btn
+        id="apply-course-action-btn"
+        class="mx-2"
+        color="secondary"
+        :disabled="disableControls || !allowEdits || !selectedCourseAction || !selectedEvaluationIds.length"
+        @click="applyCourseAction"
+        @keypress.enter.prevent="applyCourseAction"
+      >
+        Apply
+      </v-btn>
+    </div>
+    <div v-if="selectedCourseAction === 'duplicate'" class="mb-4">
+      <div class="d-flex align-center mt-2">
+        <PersonLookup
+          id="bulk-duplicate-instructor-lookup-autocomplete"
+          :instructor-lookup="true"
+          placeholder="Instructor name or UID"
+          :on-select-result="selectInstructor"
+          solo
         />
       </div>
-    </v-col>
-  </v-row>
+      <v-checkbox
+        v-model="bulkUpdateOptions.midtermFormEnabled"
+        class="text-nowrap"
+        color="tertiary"
+        :disabled="disableControls"
+        hide-details="auto"
+        label="Use midterm department forms"
+      />
+      <div class="d-flex align-center mt-2">
+        <label
+          for="bulk-duplicate-start-date"
+          class="v-label"
+          :class="$vuetify.theme.dark ? 'theme--dark' : 'theme--light'"
+        >
+          Evaluation start date:
+        </label>
+        <c-date-picker
+          v-model="bulkUpdateOptions.startDate"
+          class="mx-3"
+          :min-date="$moment($config.currentTermDates.begin).toDate()"
+          :max-date="$moment($config.currentTermDates.end).subtract(20, 'days').toDate()"
+          title-position="left"
+        >
+          <template v-slot="{ inputValue, inputEvents }">
+            <input
+              id="bulk-duplicate-start-date"
+              class="datepicker-input input-override my-0"
+              :class="$vuetify.theme.dark ? 'dark' : 'light'"
+              :disabled="disableControls"
+              maxlength="10"
+              minlength="10"
+              :value="inputValue"
+              v-on="inputEvents"
+            />
+          </template>
+        </c-date-picker>
+      </div>
+    </div>
+    <v-dialog
+      id="error-dialog"
+      v-model="errorDialog"
+      width="400"
+      role="alertdialog"
+      aria-labelledby="error-dialog-title"
+      aria-describedby="error-dialog-text"
+    >
+      <v-card>
+        <v-card-title id="error-dialog-title" tabindex="-1">Error</v-card-title>
+        <v-card-text id="error-dialog-text" class="pt-3">{{ errorDialogText }}</v-card-text>
+        <div class="d-flex pa-2">
+          <div class="mr-2">
+            <v-btn
+              id="error-dialog-ok-btn"
+              color="primary"
+              @click="dismissErrorDialog"
+              @keypress.enter.prevent="dismissErrorDialog"
+            >
+              OK
+            </v-btn>
+          </div>
+        </div>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
 import {updateEvaluations} from '@/api/departments'
-import AddCourseSection from '@/components/evaluation/AddCourseSection'
 import Context from '@/mixins/Context'
 import DepartmentEditSession from '@/mixins/DepartmentEditSession'
 import PersonLookup from '@/components/admin/PersonLookup'
@@ -150,7 +123,6 @@ import Util from '@/mixins/Util'
 export default {
   name: 'EvaluationActions',
   components: {
-    AddCourseSection,
     PersonLookup
   },
   mixins: [Context, DepartmentEditSession, Util],
@@ -183,14 +155,6 @@ export default {
     }
   },
   methods: {
-    addCourseSection(courseNumber) {
-      this.alertScreenReader(`Adding section ${courseNumber}.`)
-      this.addSection(courseNumber).then(() => {
-        this.isAddingSection = false
-        this.alertScreenReader(`Section ${courseNumber} added.`)
-      }, error => this.showErrorDialog(error.response.data.message))
-      .finally(() => this.setDisableControls(false))
-    },
     afterApply() {
       /* TODO: a more informative screen reader alert plus a visual indication of the affected row(s) */
       this.refreshAll()
@@ -219,11 +183,6 @@ export default {
         ).then(data => this.afterApply(data), error => this.showErrorDialog(error.response.data.message))
         .finally(() => this.setDisableControls(false))
       }
-    },
-    cancelAddSection() {
-      this.isAddingSection = false
-      this.alertScreenReader('Section lookup canceled.')
-      this.$putFocusNextTick('add-course-section-btn')
     },
     selectInstructor(suggestion) {
       this.instructor = suggestion
