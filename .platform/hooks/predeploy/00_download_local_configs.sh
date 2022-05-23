a74360118dd24401819924a066a758d822fef962
Pauline Kerschen <kerschen@berkeley.edu>
2022-05-23 13:51:44 -0700

    DAMIEN-163 Set up configs and authentication for SFTP connection

--- .platform/hooks/predeploy/00_download_local_configs.sh
+++ .platform/hooks/predeploy/00_download_local_configs.sh
@@ -3,3 +3,7 @@ PYTHONPATH='' aws s3 cp s3://damien-deploy-configs/damien/${EB_ENVIRONMENT}.py c
 printf "\nEB_ENVIRONMENT = '${EB_ENVIRONMENT}'\n\n" >> config/production-local.py
 chown webapp config/production-local.py
 chmod 400 config/production-local.py
+
+PYTHONPATH='' aws s3 cp s3://damien-deploy-configs/damien/${EB_ENVIRONMENT}_rsa config/damien_rsa
+chown webapp config/damien_rsa
+chmod 400 config/damien_rsa
