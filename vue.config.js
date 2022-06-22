bfdd571c0177883f75be01889ca5cb2fc6a50436
Pauline Kerschen <kerschen@berkeley.edu>
2022-06-22 11:58:43 -0700

    DAMIEN-347 Update vue-svg-loader webpack configuration

--- vue.config.js
+++ vue.config.js
@@ -5,14 +5,16 @@ module.exports ={
     'vuetify'
   ],
   chainWebpack: (config) => {
-    const svgRule = config.module.rule('svg');
-    svgRule.uses.clear();
-    svgRule
-      .use('babel-loader')
-      .loader('babel-loader')
-      .end()
-      .test(/(\?.*)?\.(svg)(\?.*)?$/)
-      .use('vue-svg-loader')
-      .loader('vue-svg-loader');
+    config.module.rules.delete('svg')
+  },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.svg$/, 
+          loader: 'vue-svg-loader', 
+        },
+      ],
+    }      
   }
 }
