5c2a1d2c572a9aa44cced9f30802a25dcfc38560
Pauline Kerschen <kerschen@berkeley.edu>
2022-06-24 15:41:53 -0700

    DAMIEN-355 Adjust date format in courses export

--- tests/api/test_evaluation_controller.py
+++ tests/api/test_evaluation_controller.py
@@ -222,7 +222,7 @@ class TestExportEvaluations:
                                   'SECTION_NUM,PRIMARY_SECONDARY_CD,EVALUATE,DEPT_FORM,EVALUATION_TYPE,MODULAR_COURSE,START_DATE,END_DATE,'
                                   'CANVAS_COURSE_ID,QB_MAPPING')
             assert courses[1] == ('2022-B-30643,2022-B-30643,"HISTORY C188C LEC 001 Magic, Religion, and Science: The Ancient and Medieval Worlds",'
-                                  'Y,30470-30643,HISTORY,C188C,LEC,001,P,Y,HISTORY,F,,04-27-2022,05-17-2022,,HISTORY-F')
+                                  'Y,30470-30643,HISTORY,C188C,LEC,001,P,Y,HISTORY,F,,4/27/22,5/17/22,,HISTORY-F')
 
             course_instructors = _read_csv(exported_objects, '/course_instructors.csv')
             assert len(course_instructors) == 2
