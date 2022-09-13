5d1f3d42994541300bdb065bfbe8cf199fd94f5b
Pauline Kerschen <kerschen@berkeley.edu>
2022-09-13 11:48:05 -0700

    DAMIEN-469 Dedupllication for xlistings and room shares too

--- damien/lib/queries.py
+++ damien/lib/queries.py
@@ -181,7 +181,7 @@ def get_loch_basic_attributes_by_uid_or_name(snippet, limit=20, exclude_uids=Non
 def get_cross_listings(term_id, course_numbers):
     if not len(course_numbers):
         return []
-    query = """SELECT
+    query = """SELECT DISTINCT
                 ss.*,
                 cl2.course_number AS cross_listed_with,
                 TRUE AS foreign_department_course
@@ -207,7 +207,7 @@ def get_cross_listings(term_id, course_numbers):
 def get_room_shares(term_id, course_numbers):
     if not len(course_numbers):
         return []
-    query = """SELECT
+    query = """SELECT DISTINCT
                 ss.*,
                 cs2.course_number AS room_shared_with,
                 TRUE AS foreign_department_course
