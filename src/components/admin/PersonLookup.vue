dd387fbb968a88f6cca481ef6d263e2f11f48d18
Anne Lyttle <alyttle@berkeley.edu>
2025-03-19 16:47:56 -0700

    DAMIEN-801: refactors person lookup a11y fixes for use in dept forms select

--- src/components/admin/PersonLookup.vue
+++ src/components/admin/PersonLookup.vue
@@ -9,100 +9,33 @@
         <span v-if="label">{{ label }} <span class="sr-only">{{ placeholder }}</span></span>
       </label>
     </div>
-    <div :id="`${idPrefix}-container`" :class="{'v-col v-col-6': inline}">
-      <v-autocomplete
-        :id="`${idPrefix}-input`"
-        ref="container"
-        v-model="selected"
-        :aria-describedby="`${idPrefix}-error`"
-        :aria-disabled="disabled"
-        :aria-labelledby="`${idPrefix}-label`"
-        autocomplete="off"
-        class="person-lookup"
-        :class="inputClass"
-        clearable
-        color="secondary"
-        base-color="secondary"
-        bg-color="white"
-        density="compact"
-        :disabled="disabled"
-        eager
-        :error="required && !suppressValidation && !!size(errors)"
-        :error-messages="required && !suppressValidation ? errors : []"
-        hide-details
-        :hide-no-data="isSearching || !query"
-        :items="suggestions"
-        :list-props="{ariaLive: 'off'}"
-        :loading="isSearching ? 'primary' : false"
-        :menu-icon="null"
-        :menu-props="{closeOnContentClick: true, id: `${idPrefix}-menu`}"
-        no-data-text="No results found."
-        no-filter
-        persistent-clear
-        :placeholder="placeholder"
-        return-object
-        :search="query"
-        :variant="variant"
-        @keydown.esc="onKeyDownEsc"
-        @update:focused="onFocusInput"
-        @update:menu="onToggleMenu"
-        @update:search="onUpdateSearch"
-      >
-        <template #loader="{isActive}">
-          <v-progress-circular
-            v-if="isActive"
-            class="mr-2"
-            color="tertiary"
-            indeterminate
-            size="x-small"
-            width="2"
-          />
-        </template>
-        <template #clear>
-          <v-btn
-            v-if="!isSearching"
-            :id="`${idPrefix}-clear-btn`"
-            :aria-label="`Clear ${label} input`"
-            :class="{'disabled-opacity': !selected}"
-            color="secondary"
-            density="compact"
-            :disabled="!selected"
-            exact
-            icon
-            :ripple="false"
-            variant="text"
-            @keydown.enter.stop.prevent="onClearInput"
-            @click.stop.prevent="onClearInput"
-          >
-            <v-icon
-              color="secondary"
-              :icon="mdiCloseCircle"
-              size="21"
-            ></v-icon>
-          </v-btn>
-        </template>
-        <template #item="{index, item}">
-          <v-list-item
-            :id="`${idPrefix}-option-${index}`"
-            :aria-selected="index === focusedListItemIndex"
-            base-color="secondary"
-            class="font-size-18 person-lookup-result"
-            @click="() => onSelectItem(item)"
-            @focus="e => onFocusListItem(e, index)"
-            @mouseenter="e => onFocusListItem(e, index)"
-          >
-            <template #title>
-              <span v-html="suggest(item)" />
-            </template>
-          </v-list-item>
-        </template>
-        <template #selection="{item}">
-          <span class="truncate-with-ellipsis">{{ getUserLabel(item.value) }}</span>
-        </template>
-      </v-autocomplete>
-    </div>
+    <AccessibleCombobox
+      :clazz="`person-lookup ${inputClass}`"
+      :container-class="{'v-col v-col-6': inline}"
+      :disabled="disabled"
+      :error="required && !suppressValidation && !!size(errors)"
+      :error-messages="required && !suppressValidation ? errors : []"
+      :filter-results="onUpdateSearch"
+      :get-value="() => selected"
+      :id-prefix="idPrefix"
+      is-autocomplete
+      :is-busy="isSearching"
+      :item-label="suggest"
+      :items="suggestions"
+      :label="label"
+      :list-label="listLabel"
+      :on-clear="onClearInput"
+      :on-key-down-esc="onKeyDownEsc"
+      :placeholder="placeholder"
+      :set-value="v => selected = v"
+      :when-item-selected="onSelectItem"
+    >
+      <template #selection="{item}">
+        <span class="truncate-with-ellipsis">{{ getUserLabel(item.value) }}</span>
+      </template>
+    </AccessibleCombobox>
     <div
-      :id="`${idPrefix}-error`"
+      :id="`${idPrefix}-desc`"
       aria-live="assertive"
       :class="{'v-col v-col-2 pl-0': inline}"
       role="alert"
@@ -119,10 +52,10 @@
 </template>
 
 <script setup>
-import {alertScreenReader, putFocusNextTick} from '@/lib/utils'
-import {debounce, delay, each, get, replace, size, split, trim} from 'lodash'
-import {mdiCloseCircle} from '@mdi/js'
-import {nextTick, onMounted, onUpdated, ref} from 'vue'
+import AccessibleCombobox from '@/components/util/AccessibleCombobox'
+import {alertScreenReader} from '@/lib/utils'
+import {debounce, delay, each, replace, size, split, trim} from 'lodash'
+import {onMounted, ref} from 'vue'
 import {pluralize} from '@/lib/utils'
 import {searchInstructors} from '@/api/instructor'
 import {searchUsers} from '@/api/user'
@@ -196,10 +129,8 @@ const props = defineProps({
   }
 })
 
-const container = ref()
 const debouncedSearch = ref(v => v)
 const errors = ref([])
-const focusedListItemIndex = ref(undefined)
 const isSearching = ref(false)
 const query = ref(undefined)
 // const selected = defineModel('selected', {default: {}, type: Object})
@@ -209,27 +140,7 @@ const suppressValidation = ref(true)
 const theme = useTheme()
 
 onMounted(() => {
-  const combobox = getComboboxElement()
-  if (combobox) {
-    combobox.removeAttribute('role')
-    combobox.removeAttribute('aria-expanded')
-  }
-  const input = getInputElement()
-  if (input) {
-    input.setAttribute('role', 'combobox')
-    input.setAttribute('aria-autocomplete', 'list')
-    input.setAttribute('aria-controls', `${props.idPrefix}-menu`)
-    input.setAttribute('aria-expanded', false)
-    input.setAttribute('aria-label', props.label)
-  }
   debouncedSearch.value = debounce(executeSearch, 300)
-})
-
-onUpdated(() => {
-  const combobox = getComboboxElement()
-  if (combobox) {
-    combobox.removeAttribute('aria-expanded')
-  }
 })
 
 const executeSearch = () => {
@@ -247,68 +158,21 @@ const executeSearch = () => {
   })
 }
 
-const getComboboxElement = () => {
-  const container = document.getElementById(`${props.idPrefix}-container`)
-  return container ? container.querySelector('.v-field') : null
-}
-
-const getInputElement = () => {
-  return document.getElementById(`${props.idPrefix}-input`)
-}
-
 const getUserLabel = user => `${user.firstName} ${user.lastName} (${user.uid})`
 
 const onClearInput = () => {
-  query.value = selected.value = null
+  query.value = null
   suggestions.value = []
   props.onSelectResult(null)
-  alertScreenReader('Cleared.')
-  putFocusNextTick(`${props.idPrefix}-input`)
 }
 
-
-const onFocusInput = isFocused => {
-  // Passing open-on-focus via menuProps (https://vuetifyjs.com/en/api/v-menu/#props-open-on-focus)
-  // doesn't seem to have an effect, thus this workaround.
-  if (props.openOnFocus && isFocused && !container.value.menu) {
-    container.value.menu = true
-  }
-}
-
-const onFocusListItem = (event, index) => {
-  const input = getInputElement()
-  input.setAttribute('aria-activedescendant', event.target.id)
-  focusedListItemIndex.value = index
-}
-
-const onSelectItem = item => {
-  selected.value = get(item.raw, 'value', item.raw)
+const onSelectItem = () => {
   validate(selected.value)
   if (!selected.value) {
     query.value = null
   }
   props.onSelectResult(selected.value)
   suggestions.value = []
-  query.value = ''
-}
-
-const onToggleMenu = isOpen => {
-  nextTick(() => {
-    const input = getInputElement()
-    if (input) {
-      if (isOpen) {
-        const menu = document.getElementById(`${props.idPrefix}-menu`)
-        const listbox = menu && menu.querySelector('[role="listbox"]')
-        if (listbox) {
-          listbox.setAttribute('aria-label', props.listLabel)
-        }
-        input.setAttribute('aria-expanded', true)
-      } else {
-        input.setAttribute('aria-expanded', false)
-        input.removeAttribute('aria-activedescendant')
-      }
-    }
-  })
 }
 
 const onUpdateSearch = q => {
@@ -340,11 +204,6 @@ const validate = suggestion => {
 }
 </script>
 
-<style>
-.person-lookup-result .highlight-match {
-  background-color: rgba(var(--v-theme-primary), var(--v-pressed-opacity));
-}
-</style>
 <style scoped>
 .autocomplete-menu {
   z-index: 210 !important;
---
+++ src/components/util/AccessibleCombobox.vue
@@ -0,0 +1,364 @@
+<template>
+  <div :id="`${idPrefix}-container`" :class="containerClass">
+    <component
+      :is="isAutocomplete ? 'v-autocomplete' : 'v-combobox'"
+      :id="`${idPrefix}-input`"
+      ref="container"
+      v-model="model"
+      :aria-describedby="`${idPrefix}-desc`"
+      :auto-select-first="!isAutocomplete"
+      autocomplete="off"
+      base-color="secondary"
+      :bg-color="isAutocomplete ? 'white' : 'surface'"
+      :class="clazz"
+      clearable
+      color="secondary"
+      density="compact"
+      :debounce="500"
+      :disabled="disabled"
+      eager
+      :error="error"
+      :error-messages="errorMessages"
+      hide-details
+      :hide-no-data="isBusy || !query"
+      :hide-selected="!isAutocomplete"
+      :item-title="itemTitle"
+      :item-value="itemValue"
+      :items="items"
+      :list-props="{ariaLive: ariaLive}"
+      :loading="isBusy"
+      :menu-icon="null"
+      :menu-props="{closeOnContentClick: true, id: `${idPrefix}-menu`}"
+      :multiple="!isAutocomplete"
+      no-data-text="No results found."
+      :no-filter="isAutocomplete"
+      persistent-clear
+      :placeholder="placeholder || label"
+      return-object
+      :search="query"
+      :variant="variant"
+      @blur.stop.prevent="onBlur"
+      @keydown.esc="onKeyDownEsc"
+      @update:focused="onFocusInput"
+      @update:menu="onToggleMenu"
+      @update:search="onUpdateSearch"
+    >
+      <template #loader="{isActive}">
+        <v-progress-circular
+          v-if="isActive"
+          class="mr-2"
+          color="tertiary"
+          indeterminate
+          size="x-small"
+          width="2"
+        />
+      </template>
+      <template #clear>
+        <v-btn
+          v-if="!isBusy"
+          :id="`${idPrefix}-clear-btn`"
+          :aria-label="`Clear ${label} input`"
+          :class="{'disabled-opacity': !model}"
+          color="secondary"
+          density="compact"
+          :disabled="!model"
+          exact
+          icon
+          :ripple="false"
+          variant="text"
+          @keydown.enter.stop.prevent="onClearInput"
+          @click.stop.prevent="onClearInput"
+        >
+          <v-icon
+            color="secondary"
+            :icon="mdiCloseCircle"
+            size="21"
+          ></v-icon>
+        </v-btn>
+      </template>
+      <template #item="{index, item}">
+        <v-list-item
+          :id="`${idPrefix}-option-${index}`"
+          :aria-posinset="index"
+          :aria-selected="index === focusedListItemIndex"
+          :aria-setsize="itemCount || size(items)"
+          base-color="secondary"
+          class="font-size-18 autocomplete-list-item"
+          role="option"
+          @click="() => onSelectItem(item)"
+          @focus="e => onFocusListItem(e, index)"
+          @mouseenter="e => onFocusListItem(e, index)"
+        >
+          <template #title>
+            <span v-html="itemLabel(item)" />
+          </template>
+        </v-list-item>
+      </template>
+      <template #selection="{item}">
+        <slot name="selection" :item="item"></slot>
+      </template>
+    </component>
+  </div>
+</template>
+
+<script setup>
+import {alertScreenReader, pluralize, putFocusNextTick} from '@/lib/utils'
+import {get, filter, includes, isEmpty, size} from 'lodash'
+import {mdiCloseCircle} from '@mdi/js'
+import {nextTick, onMounted, onUpdated, ref} from 'vue'
+
+const props = defineProps({
+  ariaLive: {
+    default: 'off',
+    required: false,
+    type: String
+  },
+  clazz: {
+    default: '',
+    required: false,
+    type: [String, Object]
+  },
+  containerClass: {
+    default: '',
+    required: false,
+    type: [String, Object]
+  },
+  disabled: {
+    required: false,
+    type: Boolean
+  },
+  error: {
+    required: false,
+    type: Boolean
+  },
+  errorMessages: {
+    default: () => [],
+    required: false,
+    type: Array
+  },
+  filterResults: {
+    default: () => {},
+    required: false,
+    type: Function
+  },
+  getValue: {
+    required: true,
+    type: Function
+  },
+  idPrefix: {
+    required: true,
+    type: String
+  },
+  isAutocomplete: {
+    required: false,
+    type: Boolean
+  },
+  isBusy: {
+    required: false,
+    type: Boolean
+  },
+  itemCount: {
+    default: undefined,
+    required: false,
+    type: Number
+  },
+  itemLabel: {
+    default: v => v,
+    required: false,
+    type: Function
+  },
+  itemTitle: {
+    default: 'title',
+    required: false,
+    type: String
+  },
+  itemValue: {
+    default: 'value',
+    required: false,
+    type: String
+  },
+  items: {
+    required: true,
+    type: Array
+  },
+  label: {
+    required: true,
+    type: String
+  },
+  listLabel: {
+    required: true,
+    type: String
+  },
+  onClear: {
+    default: () => {},
+    required: false,
+    type: Function
+  },
+  onKeyDownEsc: {
+    default: () => {},
+    required: false,
+    type: Function
+  },
+  placeholder: {
+    default: undefined,
+    required: false,
+    type: String
+  },
+  setValue: {
+    required: true,
+    type: Function
+  },
+  variant: {
+    default: 'outlined',
+    required: false,
+    type: String
+  },
+  whenItemSelected: {
+    default: () => {},
+    required: false,
+    type: Function
+  }
+})
+
+const container = ref()
+const focusedListItemIndex = ref(undefined)
+const model = defineModel({
+  get() {
+    return props.getValue()
+  },
+  set(v) {
+    props.setValue(v)
+  },
+  type: String
+})
+const query = ref(undefined)
+const resultsSummary = ref(undefined)
+const resultsSummaryInterval = ref(undefined)
+
+onMounted(() => {
+  const combobox = getComboboxElement()
+  if (combobox) {
+    combobox.removeAttribute('role')
+    combobox.removeAttribute('aria-expanded')
+  }
+  const input = getInputElement()
+  if (input) {
+    input.setAttribute('role', 'combobox')
+    input.setAttribute('aria-autocomplete', 'list')
+    input.setAttribute('aria-controls', `${props.idPrefix}-menu`)
+    input.setAttribute('aria-expanded', false)
+    input.setAttribute('aria-label', props.label)
+  }
+})
+
+onUpdated(() => {
+  nextTick(() => {
+    const combobox = getComboboxElement()
+    if (combobox) {
+      const menuId = combobox.getAttribute('aria-owns')
+      combobox.removeAttribute('aria-expanded')
+      if (menuId) {
+        combobox.setAttribute('aria-controls', menuId)
+        combobox.removeAttribute('aria-owns')
+      }
+    }
+  })
+})
+
+const getComboboxElement = () => {
+  const container = document.getElementById(`${props.idPrefix}-container`)
+  return container ? container.querySelector('.v-field') : null
+}
+
+const getInputElement = () => {
+  return document.getElementById(`${props.idPrefix}-input`)
+}
+
+const onBlur = () => {
+  const input = getInputElement()
+  input.removeAttribute('aria-activedescendant')
+  focusedListItemIndex.value = null
+  if (isEmpty(query.value)) {
+    props.onClear()
+  }
+}
+
+const onClearInput = () => {
+  model.value = null
+  props.onClear()
+  alertScreenReader('Cleared.')
+  putFocusNextTick(`${props.idPrefix}-input`)
+}
+
+const onFocusInput = isFocused => {
+  if (isFocused) {
+    const input = getInputElement()
+    input.removeAttribute('aria-activedescendant')
+    focusedListItemIndex.value = null
+    // Passing open-on-focus via menuProps (https://vuetifyjs.com/en/api/v-menu/#props-open-on-focus)
+    // doesn't seem to have an effect, thus this workaround.
+    if (props.openOnFocus && !container.value.menu) {
+      container.value.menu = true
+    }
+  }
+}
+
+const onFocusListItem = (event, index) => {
+  const input = getInputElement()
+  input.setAttribute('aria-activedescendant', event.target.id)
+  focusedListItemIndex.value = index
+}
+
+const onSelectItem = item => {
+  model.value = get(item.raw, 'value', item.raw)
+  if (!model.value) {
+    query.value = null
+  }
+  nextTick(props.whenItemSelected)
+}
+
+const onToggleMenu = isOpen => {
+  nextTick(() => {
+    const input = getInputElement()
+    if (input) {
+      if (isOpen) {
+        const menu = document.getElementById(`${props.idPrefix}-menu`)
+        const listbox = menu && menu.querySelector('[role="listbox"]')
+        if (listbox) {
+          listbox.setAttribute('aria-label', props.listLabel)
+        }
+        input.setAttribute('aria-expanded', true)
+      } else {
+        input.setAttribute('aria-expanded', false)
+        input.removeAttribute('aria-activedescendant')
+        clearInterval(resultsSummaryInterval.value)
+      }
+    }
+  })
+}
+
+const onUpdateSearch = q => {
+  query.value = q
+  props.filterResults(q)
+  clearInterval(resultsSummaryInterval.value)
+  resultsSummaryInterval.value = setInterval(setResultsSummary, 1000)
+}
+
+const setResultsSummary = () => {
+  const menuOverlay = document.getElementById(`${props.idPrefix}-menu`)
+  const listbox = menuOverlay && menuOverlay.querySelector('[role="listbox"]')
+  clearInterval(resultsSummaryInterval.value)
+  if (listbox) {
+    const suggestions = filter(listbox.children, child => includes(child.classList, 'v-list-item'))
+    resultsSummary.value = pluralize('result', suggestions.length)
+  } else {
+    resultsSummary.value = ''
+  }
+}
+</script>
+
+<style>
+.autocomplete-list-item .highlight-match {
+  background-color: rgba(var(--v-theme-primary), var(--v-pressed-opacity));
+}
+</style>
