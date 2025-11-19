572b336d34d3f838f539648f558f0bb6dc2c2f12
John Crossman <johncrossman@berkeley.edu>
2025-11-19 11:25:32 -0800

    DAMIEN-834, remove old Vue2 artifacts incl. src-vue2 dir

--- src-vue2/api/sections.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-import Vue from 'vue'
-
-export function getSection(courseNumber: string, termId: string) {
-  return axios.get(`${Vue.prototype.$config.apiBaseUrl}/api/section/${courseNumber}?term_id=${termId}`)
-}
